bba8003b73961f3632d16f8c535cbb49e947530d
Kunj Patel <kunjpatel020304@gmail.com>
2025-03-15 23:55:40 +0530

    service committed

---
+++ backend/src/main/java/com/example/backend/service/EmailService.java
@@ -0,0 +1,39 @@
+package com.eventmatch.eventmatchproject.service;
+
+import com.eventmatch.eventmatchproject.models.Auction;
+import com.eventmatch.eventmatchproject.models.User;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            mailSender.send(message);
+        } catch (Exception e) {
+            System.err.println("Error sending email: " + e.getMessage());
+        }
+    }
+
+    public void sendOtpEmail(String to, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("Your OTP for Registration");
+        message.setText("Your OTP for registration is: " + otp +
+                "\nThis OTP is valid for 10 minutes.");
+
+        mailSender.send(message);
+    }
+}
